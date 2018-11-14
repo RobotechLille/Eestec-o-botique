53c7c644e42b0e87c2ade99a652352e599ad8e47
pfrison <pierre.frison@polytech-lille.net>
2018-11-14 23:11:38 +0100

    Revert "Mise à jour 1.1"
    This reverts commit b041205e4456f878a0a7d3ade3ab5b3a55d6f05e.

--- src/main/java/me/robotech/eestecobotique/Modes/ModeActivity.java
+++
@@ -1,8 +0,0 @@
-package me.robotech.eestecobotique.Modes;
-
-import android.support.v7.app.AppCompatActivity;
-
-public abstract class ModeActivity extends AppCompatActivity {
-    public int gauche = 127;
-    public int droite = 127;
-}
